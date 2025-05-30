6c26f6787ebc0ddc3d40dd39ce9478d1033ad9a5
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-30 19:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click',()=>{
+	alert('You clicked OK. Proceeding...')
+})
+
+document.getElementById('btncancel').addEventListener('click',()=>{
+	alert('You clicked Cancel. Exiting...')
+})
